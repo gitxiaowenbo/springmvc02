121aa47de248b491076aa47ee18295e99330cf55
xiao <xiao>
2017-06-10 23:42:16 +0800

    init

---
+++ src/main/java/com/springdemo/controller/DemoController.java
@@ -0,0 +1,14 @@
+package com.springdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/demo")
+public class DemoController {
+
+    @RequestMapping("/indexw")
+    public String index(){
+        return "demo";
+    }
+}
